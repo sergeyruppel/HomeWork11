8da9440e6c95ebb10f3b203c5b63126d75c76034
Sergey Ruppel <sergeyruppel@users.noreply.github.com>
2023-08-09 17:08:46 +0700

    Project created

---
+++ HomeWork11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork11
+//
+//  Created by Sergey Ruppel on 09.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
